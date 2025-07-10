baee8c4a569fa0881842d12f3c2240ff13be9de6
TheRealKuchen <111386628+TheRealKuchen@users.noreply.github.com>
2025-07-10 20:22:35 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -29,37 +29,5 @@ Config.Objects = {
     ['barrier'] = {
         label = 'Absperrung',
         model = 'prop_barrier_work06a',
-    },
-    ['colle_alien'] = {
-        label = 'Alien',
-        model = 'vw_prop_vw_colle_alien',
-    },
-    ['colle_beast'] = {
-        label = 'Beast',
-        model = 'vw_prop_vw_colle_beast',
-    },
-    ['colle_bigfoot'] = {
-        label = 'Bigfoot',
-        model = 'vw_prop_vw_colle_sasquatch',
-    },
-    ['colle_imporage'] = {
-        label = 'Imporage',
-        model = 'vw_prop_vw_colle_imporage',
-    },
-    ['colle_pogo'] = {
-        label = 'Pogo',
-        model = 'vw_prop_vw_colle_pogo',
-    },
-    ['colle_prbubble'] = {
-        label = 'Prbubble',
-        model = 'vw_prop_vw_colle_prbubble',
-    },
-    ['colle_rsrcomm'] = {
-        label = 'Rsrcomm',
-        model = 'vw_prop_vw_colle_rsrcomm',
-    },
-    ['colle_rsrgeneric'] = {
-        label = 'Rsrgeneric',
-        model = 'vw_prop_vw_colle_rsrgeneric',
     }
 }
